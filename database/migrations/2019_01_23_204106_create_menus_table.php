7125866d6d7f4a79f64838ad706e5637fa049312
Ali Ghasemzadeh <it.ghasemzadeh@gmail.com>
2019-01-23 20:45:37 +0330

    Add Menu Options for new versions.

---
+++ database/migrations/2019_01_23_204106_create_menus_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('type');
+            $table->string('position'); //adminsidebar,usersidebar,footer,header,userdrop,
+            $table->string('color')->nullable();
+            $table->string('image')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('route')->nullable();
+            $table->string('link')->nullable();
+            $table->string('order')->nullable();
+            $table->integer('menu_id')->nullable();
+            $table->boolean('enabled');
+            $table->longText('options')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
